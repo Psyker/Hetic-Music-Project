a618e1023fe18d1ecde58816760ea24796ecb986
Psyker <theo.bourgoin@hetic.net>
2016-07-01 14:18:04 +0200

    fix js

--- web/js/main.js
+++ web/js/main.js
@@ -1,25 +1,25 @@
 // Charger les audios en JS
+function playAllSounds(audios) {
+    $.each(audios, function (key, el) {
+        el.play();
+    });
+}
 
-var snd1 = new Audio();
-var src1 = document.createElement("source");
-src1.type = "audio/mp3";
-src1.src = "fallen-exemple-play/fallen/fallen2.mp3";
-snd1.appendChild(src1);
-snd1.loop = true;
+function pauseAllSounds(audios) {
+    $.each(audios, function (key, el) {
+        el.pause();
+    });
+}
 
-var snd2 = new Audio();
-var src2 = document.createElement("source");
-src2.type = "audio/mp3";
-src2.src = "fallen-exemple-play/fallen/fallenettajames.mp3";
-snd2.appendChild(src2);
-snd2.loop = true;
+function pauseAll(video, audios) {
+    video.pause();
+    pauseAllSounds(audios);
+}
 
-var snd3 = new Audio();
-var src3 = document.createElement("source");
-src3.type = "audio/mp3";
-src3.src = "fallen-exemple-play/fallen/fallenronetrash.mp3";
-snd3.appendChild(src3);
-snd3.loop = true;
+function playAll(video, audios) {
+    video.play();
+    playAllSounds(audios);
+}
 
 function muteSong(el) {
     el.muted = true;
@@ -37,83 +37,84 @@ function playSong(el) {
     el.play();
 }
 
-// play - stop au click
-
-
 $(document).ready(function() {
+    var video = document.getElementById("myVid"),
+        audios = {};
 
-    $("#owl-example").owlCarousel({
+    $(".sound").each(function (key, el) {
+        var $el = $(el);
+        var id = $el.data("id"),
+            src = $el.data("src"),
+            sound = new Audio(),
+            source = document.createElement("source");
 
-    });
+        source.type = "audio/mp3";
+        source.src = src;
+        sound.appendChild(source);
+        sound.loop = true;
 
-    playSong(snd1);
-    muteSong(snd2);
-    playSong(snd2);
-    muteSong(snd3);
-    playSong(snd3);
+        audios[id] = sound;
 
-    $("#myVid").on('click', function () {
-        var video = document.querySelector('#myVid');
+        playSong(sound);
 
-        if (video.paused) {
-            video.play();
-            snd1.play();
-            snd2.play();
-            snd3.play();
-        } else {
-            video.pause();
-            snd1.pause();
-            snd2.pause();
-            snd3.pause();
+        if (key != 0) {
+            muteSong(sound);
         }
     });
 
-    $(".player").on('click', function() {
-        var video = document.querySelector('#myVid');
+    /*$("#owl-example").owlCarousel({
+
+    });*/
+
+    $(video).click(function () {
+        if (video.paused) {
+            video.play();
+            playAllSounds(audios);
+        } else {
+            video.pause();
+            pauseAllSounds(audios);
+        }
+    });
+
+    $(".player").click(function() {
         $(this).toggleClass("player2");
 
         if (video.paused) {
-            video.play();
-            snd1.play();
-            snd2.play();
-            snd3.play();
+            playAll(video, audios);
         } else {
-            video.pause();
-            snd1.pause();
-            snd2.pause();
-            snd3.pause();
+            pauseAll(video, audios);
         }
     });
 
     // mute functions
 
-    $(".sound").on('click', function() {
-        var id = $(this).data("id");
-        $(this).addClass("active");
-        window["snd" + id].muted = false;
+    $(".sound").click(function () {
+        var $this = $(this);
+        var id = $this.data("id");
 
-        $(".sound[data-id!=" + id + "]").each(function(key, el) {
+        $this.addClass("active");
+
+        audios[id].muted = false;
+
+        $(".sound[data-id!=" + id + "]").each(function (key, el) {
             var currentId = $(el).data("id");
             $(this).removeClass("active");
 
-            window["snd" + currentId].muted = true;
+            audios[currentId].muted = true;
         });
     });
 
     // Synchroniser restart loop video et son
-    var videoLoop = document.querySelector("#myVid");
-    videoLoop.addEventListener('ended', function() {
-        snd1.currentTime = 0;
-        snd2.currentTime = 0;
-        snd3.currentTime = 0;
+    video.addEventListener("ended", function() {
+        $.each(audios, function (key, el) {
+            el.currentTime = 0;
+            el.play();
+        });
+
         this.play();
-        snd1.play();
-        snd2.play();
-        snd3.play();
     }, false);
 
     //vidéo en plein ecran
-
     $(".full-screen").on('click', function() {
         var elem = document.getElementById("myVid");
         if (elem.requestFullscreen) {
@@ -124,4 +125,32 @@ $(document).ready(function() {
             elem.webkitRequestFullscreen();
         }
     });
+
+});
+
+// SLIDER
+
+function loopNext(){
+    $('#sliderWrapper').stop().animate({scrollLeft:'+=60'}, 'fast', 'linear', loopNext);
+}
+
+function loopPrev(){
+    $('#sliderWrapper').stop().animate({scrollLeft:'-=60'}, 'fast', 'linear', loopPrev);
+}
+
+function stop(){
+    $('#sliderWrapper').stop();
+}
+
+
+$('#next').hover(function () {
+    loopNext();
+},function () {
+    stop();
+});
+
+$('#prev').hover(function () {
+    loopPrev();
+},function () {
+    stop();
 });
